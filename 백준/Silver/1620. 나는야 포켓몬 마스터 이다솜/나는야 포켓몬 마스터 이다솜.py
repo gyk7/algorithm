fecb85d9bf7997c05dd5243ee0fee419aecd3ab9
gyk7 <62199840+gyk7@users.noreply.github.com>
2023-12-26 19:59:18 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 4920 ms, Memory: 139252 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -0,0 +1,15 @@
+N, M = map(int, input().split())
+
+idx_to_name = {}
+name_to_idx = {}
+for i in range(1, N + 1):
+    name = input().rstrip()
+    idx_to_name[i] = name
+    name_to_idx[name] = i
+
+for j in range(M) :
+    q = input().rstrip()
+    if q.isdigit():
+        print(idx_to_name[int(q)])
+    else :
+        print(name_to_idx[q])
