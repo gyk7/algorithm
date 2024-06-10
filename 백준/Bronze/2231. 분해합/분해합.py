cb9300e667737128c656d46638ef64f9801ad354
gyk7 <62199840+gyk7@users.noreply.github.com>
2024-06-10 17:03:14 +0900

    [Bronze II] Title: 분해합, Time: 1092 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,16 +1,11 @@
 N = int(input())
-li = []
 
-for a in range(10) :
-    for b in range(10) :
-        for c in range(10) :
-            for d in range(10) :
-                for e in range(10) :
-                    for f in range(10) :
-                        for g in range(10) :
-                            if ( 1000001 *a +100001 *b +10001 * c + 1001 * d + 101 *e + 11 * f + 2 * g) == N :
-                                li.append( 1000000 *a +100000 *b +10000 * c + 1000 * d + 100 *e + 10 * f + g) 
-if li:
-    print(min(li))
-else:
+for i in range(N) :
+    s = i + sum(map(int, str(i)))
+
+    if s == N :
+        print(i)
+        break
+
+else :
     print(0)
