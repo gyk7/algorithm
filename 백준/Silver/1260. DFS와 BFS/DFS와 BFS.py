8e1ad8d1174dbc4c92dc1f17b9f87f52941fd8c9
gyk7 <62199840+gyk7@users.noreply.github.com>
2024-10-25 17:12:43 +0900

    [Silver II] Title: DFS와 BFS, Time: 292 ms, Memory: 32236 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,34 +1,40 @@
-N, M, V = map(int, input().split())
+def dfs(c) :
+    visited_dfs[c] = 1
+    ans_dfs.append(c)
+    for n in adj[c] :
+        if visited_dfs[n] == 0 :
+            dfs(n)
 
-graph = [[0] * (N+1) for _ in range(N+1)]
-visited1 = [0] * (N+1)
-visited2 = [0] * (N+1)
-for _ in range(M):
-    p, q = map(int,input().split())
+def bfs(s) :
+    q = []
     
-    graph[p][q] = graph[q][p] = 1
+    q.append(s)
+    ans_bfs.append(s)
+    visited_bfs[s] = 1
+    
+    while q :
+        n = q.pop(0)
+        for i in adj[n] :
+            if not visited_bfs[i]:
+                q.append(i)
+                ans_bfs.append(i)
+                visited_bfs[i] = 1
+            
 
-def dfs(V):
-    visited1[V] = 1
+N, M, V = map(int, input().split())
+adj = [[] * (N+1) for _ in range(N + 1)]
+visited_dfs = [0] * (N + 1)
+visited_bfs = [0] * (N + 1)
+ans_dfs = []
+ans_bfs = []
+for _ in range(M):
+    s, e = map(int, input().split())
+    adj[s].append(e)
+    adj[e].append(s)
 
-    print(V, end=' ')
-    for i in range(1, N+1):
-        if graph[V][i] == 1 and visited1[i] == 0:
-            dfs(i)
-
-def bfs(V):
-    visited2[V] = 1
-    deque=[V]
-    while deque:
-        V = deque.pop(0)
-        print(V, end=' ')
-        for i in range(1, N+1):
-            if graph[V][i] ==1 and visited2[i] == 0:
-                deque.append(i)
-                visited2[i] = 1
-        
-
-  
+for i in range(1, N+1) :
+    adj[i].sort()
 dfs(V)
-print()
+print(*ans_dfs)
 bfs(V)
+print(*ans_bfs)
