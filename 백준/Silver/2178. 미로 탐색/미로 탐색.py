407a3b34f9b52af50dca097d60e77d12a434db25
gyk7 <62199840+gyk7@users.noreply.github.com>
2024-10-25 17:50:01 +0900

    [Silver I] Title: 미로 탐색, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,32 +1,26 @@
-from collections import deque
+def bfs(si, sj, ei, ej) :
+    q = []
+    v = [[0] * M for _ in range(N)]
+    
+    q.append((si, sj))
+    v[si][sj] = 1
+    
+    while q :
+        ci, cj = q.pop(0)
+        if (ci, cj) == (ei, ej) :
+            return v[ei][ej]
+        
+        for di, dj in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
+            ni, nj = ci + di, cj + dj
+            if 0<=ni<N and 0<=nj<M and arr[ni][nj] == 1 and v[ni][nj] == 0 :
+                q.append((ni, nj))
+                v[ni][nj] = v[ci][cj] + 1
+                
+    
+    
 
 N, M = map(int, input().split())
-graph = []
-visited = [[0]*M for _ in range(N)]
-for _ in range(N):
-    k = list(map(int,input()))
-    graph.append(k)
-# print(graph)
-# print(visited)
+arr = [list(map(int, input())) for _ in range(N)]
 
-def bfs():
-    # 상, 하, 좌, 우
-    dx = [0, 0, -1, 1]
-    dy = [-1, 1, 0, 0]
-
-    queue = deque()
-    queue.append((0, 0))
-    while queue : 
-        x, y = queue.popleft()   
-        for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
-            # print(f"테스트 : {nx} {ny} ")
-            if nx < N and nx >= 0 and ny < M and ny >= 0 and graph[nx][ny] == 1 and visited[nx][ny] == 0 :
-                graph[nx][ny] = graph[x][y] + 1
-                visited[nx][ny] = 1
-                queue.append((nx, ny))
-                
-    return graph[N-1][M-1]
-
-print(bfs())
+ans = bfs(0, 0, N-1, M-1)
+print(ans)
