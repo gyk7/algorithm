90014a1fb6464dce7dd600fa4d6aead51f27c67b
gyk7 <62199840+gyk7@users.noreply.github.com>
2024-10-27 02:33:46 +0900

    [Silver II] Title: 촌수계산, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.py
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -1,31 +1,25 @@
-import sys
-sys.setrecursionlimit(10000)
-
-n = int(input())
-p, q = map(int, input().split())
-m = int(input())
-graph = [[0]* (n+1) for _ in range(n+1)] 
-visited = [0] * (n+1)
-result =[]
-
-for _ in range(m):
+def bfs(s, e) :
+    q = []
+    q.append(s)
+    v[s] = 1
+    while q :
+        c = q.pop(0)
+        if c == e :
+            return v[e] - 1
+        for k in adj[c] :    
+            if not v[k] :
+                q.append(k)
+                v[k] = v[c] + 1
+                
+    return -1
+N = int(input())
+P, Q = map(int, input().split())
+M = int(input())
+adj = [[0] *(M + 1) for _ in range(N + 1)]
+v = [0] * (N + 1)
+for _ in range(M):
     x, y = map(int, input().split())
-    graph[x][y] = graph[y][x] = 1
-# print(graph)
-
-
-def dfs(V,ans):
-
-    visited[V] = 1
-    
-    
-    if V== q :
-        print(ans)
-        exit()
-    for i in range(1, n+1):
-        if graph[V][i] == 1 and visited[i] == 0 :
-            dfs(i, ans+1)
-    return -1     
-   
-
-print(dfs(p, 0))
+    adj[x].append(y)
+    adj[y].append(x)
+print(bfs(P, Q))
+# print(adj)
