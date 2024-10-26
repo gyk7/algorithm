f74ac3934bf5e12cce45d05aa70e6cddaa3650fb
gyk7 <62199840+gyk7@users.noreply.github.com>
2024-10-27 01:37:11 +0900

    [Silver I] Title: 단지번호붙이기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,71 +1,27 @@
-from collections import deque
+def bfs(si, sj) :
+    q = []
+    q.append((si, sj))
+    v[si][sj] = 1
+    cnt = 1
+    while q :
+        ci, cj = q.pop(0)
+        for di, dj in ((-1, 0), (1, 0), (0, 1), (0, -1)) :
+            ni, nj = ci + di, cj + dj
+            if 0<=ni<N and 0<=nj<N and v[ni][nj] == 0 and arr[ni][nj] == 1 :
+                cnt += 1
+                q.append((ni, nj))
+                v[ni][nj] = 1
+    return cnt            
 
 N = int(input())
+arr = [list(map(int, input())) for _ in range(N)]
 
-graph = []
-one_li = []
-house_li = []
+v = [[0] * N for _ in range(N)]
+ans = []
 
-for _ in range(N):
-    s = list(map(int,input()))
-    graph.append(s)
-    one_li.append([i for i, value in enumerate(s) if value == 1])
-    one_li = list(one_li)
-# print(one_li)
-
-def bfs():
-    dx=[0, 0, -1, 1]
-    dy=[-1, 1, 0, 0]
-    queue = deque()
-    
-    
-    while len(sum(one_li, [])) != 0 : 
-        # print(f"one_li : {one_li}")
-        for i in range(0, N) :
-            # print(f"i :{i}")
-            
-             
-            if len(one_li[i]) != 0 :
-                # print(f"len(one_li[i]):{len(one_li[i])}")
-                queue.append([i, one_li[i][0]])
-                
-                cnt = 1
-                while queue :
-                    # print(f"queue : {queue}")
-                    x,y = queue.popleft()
-                    # print(f"x :{x}, y: {y}")
-                    graph[x][y] = 0
-                    if len(one_li) != 0 :
-                        # print(f"one_li :{one_li}")
-                        one_li[x].remove(y)
-        
-                   
-                    # print(f"one_li : {one_li}")
-                    for k in range(4):
-                        nx = x + dx[k]
-                        ny = y + dy[k]
-                        # print(f"test {k} {nx} {ny}")
-                        if nx >= 0 and nx < N and ny >= 0 and ny < N :
-                            if graph[nx][ny] == 1 and [nx,ny] not in queue: 
-                                queue.append([nx, ny])
-                                cnt+=1
-                                # print(f"cnt : {cnt}")
-                        else :
-                            continue
-                    # print(f"{graph}")    
-                house_li.append(cnt)
-            house_li.sort()
-        
-        house = ""
-        
-    if len(house_li) != 0 :    
-        for k in house_li:
-            house = house + str(k) + "\n"
-    
-        
-        return  str(len(house_li)) + "\n" + house.rstrip("\n")
-    
-    else :
-        return str(0)
-
-print(bfs())
+for i in range(N) :
+    for j in range(N) :
+        if arr[i][j]== 1 and v[i][j] == 0 :
+            ans.append(bfs(i, j))
+ans.sort()
+print(len(ans), *ans, sep = "\n")
