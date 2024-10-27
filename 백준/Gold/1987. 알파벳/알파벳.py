84e7dc369fa0e333e4d65bcf93e5168802fff0ec
gyk7 <62199840+gyk7@users.noreply.github.com>
2024-10-27 16:37:24 +0900

    [Gold IV] Title: 알파벳, Time: 4180 ms, Memory: 152896 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -0,0 +1,20 @@
+def dfs(ci, cj, cnt) :
+    global ans
+    
+    ans = max(ans, cnt)
+    for di, dj in ((-1,0),(1,0),(0,1),(0,-1)) :
+        ni, nj = ci + di, cj + dj
+        if 0<=ni<R and 0<=nj<C and v[ord(arr[ni][nj])] == 0 :
+            v[ord(arr[ni][nj])] = 1
+            dfs(ni, nj, cnt +1)
+            v[ord(arr[ni][nj])] = 0       
+           
+R, C = map(int, input().split())
+arr = list(input() for _ in range(R))
+
+ans = 1
+v = [0] * 128
+
+v[ord(arr[0][0])] = 1
+dfs(0, 0, 1)
+print(ans)
