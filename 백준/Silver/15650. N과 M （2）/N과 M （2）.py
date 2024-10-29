28dab1eb195d7c86d672acdf930ea0c4d62ae6a0
gyk7 <62199840+gyk7@users.noreply.github.com>
2024-10-29 16:13:26 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,25 +1,14 @@
+def dfs(n, lst) :
+    if n > N :
+        if len(lst) == M :
+            ans.append(lst)
+        return
+    
+    dfs(n+1, lst + [n]) # 포함하는 경우
+    dfs(n+1, lst)   # 포함하지 않는 경우
+
 N, M = map(int, input().split())
-
-visited = [False] * (N + 1)
-result = []
-result_li = []
-def backtracking(result, K) :
-    if K == M :
-        result_li.append(sorted(result))
-        return 
-    for i in range(1, N+1):
-        if visited[i] == False :
-            result.append(i)
-            visited[i] = True
-            backtracking(result, K+1)
-            result.pop()
-            visited[i] = False
-        
-backtracking([], 0)
-
-result_li = list(set(map(tuple, result_li)))
-
-result_li.sort()
-
-for j in result_li :
-    print(" ".join(map(str, j)))
+ans = []
+dfs(1,[])
+for lst in ans :
+    print(*lst)
