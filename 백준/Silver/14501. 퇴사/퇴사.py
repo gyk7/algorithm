6daeb8211a56f1dbb65cdb419aa53b7358add895
gyk7 <62199840+gyk7@users.noreply.github.com>
2024-11-07 17:40:47 +0900

    [Silver III] Title: 퇴사, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,13 +1,3 @@
-# 백트래킹
-def dfs(n, sm) :
-    global ans
-    if n >= N : # 종료조건
-        ans = max(ans, sm)
-        return
-    if n + T[n] <= N : # 상담
-        dfs(n+T[n], sm+P[n])
-    dfs(n+1, sm)
-
 N = int(input())
 T = [0] * N
 P = [0] * N
@@ -15,6 +5,10 @@ P = [0] * N
 for i in range(N) :
     T[i], P[i] = map(int, input().split())
 
-ans = 0
-dfs(0, 0)
-print(ans)
+dp = [0] * (N+1)
+for n in range(N-1, -1, -1) :   # 뒤에서 앞으로(완료기준)
+    if n + T[n] <= N :          # 기간 내 상담완료 가능
+        dp[n] = max(dp[n+T[n]]+P[n], dp[n+1])
+    else :
+        dp[n] = dp[n+1]
+print(dp[0])
