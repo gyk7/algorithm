397ad29363b67cfa5450fe748c9311703112f284
gyk7 <62199840+gyk7@users.noreply.github.com>
2024-11-08 15:30:38 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 185 ms, Memory: 49,780 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
@@ -1,11 +1,28 @@
-for t in range(1, 11):
-    n = int(input())
-    print(f'#{n}', end=' ')
+#import sys
+#sys.stdin = open("input(3).txt", "r")
+
+for test_case in range(1, 11):
+    li = []
+ 
+    N = int(input())
+    col = 0
+    row = 0
+    left = 0
+    right = 0
     arr = [list(map(int, input().split())) for _ in range(100)]
-    arr_r = list(zip(*arr))
-    result, s1, s2 = 0, 0, 0
-    for i in range(100):
-        s1 += arr[i][i]
-        s2 += arr_r[i][99-i]
-        result = max(sum(arr[i]), sum(arr_r[i]), result)
-    print(max(result, s1, s2))
+    arr1 = list(map(list, zip(*arr)))
+    for j in range(100) :
+        row = sum(arr[j])
+        li.append(row)
+    for i in range(100) :
+        col = sum(arr1[i])
+        li.append(col)
+        
+    for i in range(100) :
+        left += arr[i][i]
+    li.append(left)
+    
+    for i in range(100) :
+        right += arr[i][99-i]
+    li.append(right)
+    print(f"#{N} {max(li)}")
